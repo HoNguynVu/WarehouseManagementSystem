2509c5ee2057dd5a436c878ce4b9fcdb0d0fbe74
Hồ Nguyên Vũ <23521805@gm.uit.edu.vn>
2026-04-07 15:42:37 +0700

    add unit of works

---
+++ Infracstructure/Data/UnitOfWorks.cs
@@ -0,0 +1,78 @@
+﻿using Domain.Interfaces;
+using Infracstructure.Repositories;
+using Infracstructure.UnitOfWorks;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infracstructure.Data
+{
+    public class UnitOfWorks : IAuthUow
+    {
+        private readonly IdentityDbContext _context;
+        private IDbContextTransaction? _transaction;
+
+        public IAccountRepository Accounts { get; }
+        public IOtpRepository Otps { get; }
+        public IRefreshTokenRepository RefreshTokens { get; }
+
+        public UnitOfWorks(IdentityDbContext context, IDbContextTransaction? transaction)
+        {
+            _context = context;
+            _transaction = transaction;
+
+            Accounts = new AccountRepository(_context);
+            Otps = new OtpRepository(_context);
+            RefreshTokens = new RefreshTokenRepository(_context);
+        }
+
+        public async Task BeginTransactionAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
+        {
+            if (_transaction == null)
+            {
+                _transaction = await _context.Database.BeginTransactionAsync(isolationLevel);
+            }
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_transaction == null)
+            {
+                _transaction = await _context.Database.BeginTransactionAsync();
+            }
+        }
+
+        public async Task CommitAsync()
+        {
+            if (_transaction == null)
+            {
+                throw new InvalidOperationException("No transaction in progress.");
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                await _transaction.CommitAsync();
+            }
+            catch
+            {
+                await _transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task RollbackAsync()
+        {
+            if (_transaction == null)
+            {
+                throw new InvalidOperationException("No transaction in progress.");
+            }
+            await _transaction.RollbackAsync();
+        }
+    }
+}
---
+++ Infracstructure/UnitOfWorks/IAuthUow.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using SharedLibrary.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infracstructure.UnitOfWorks
+{
+    public interface IAuthUow : ITransactionManager
+    {
+        IAccountRepository Accounts { get; }
+        IOtpRepository Otps { get; }
+        IRefreshTokenRepository RefreshTokens { get; }
+    }
+}
