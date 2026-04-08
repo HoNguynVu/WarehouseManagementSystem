36959169af53ad6fc2e402e82a9f411fffc19513
quangvinh263 <nquangvinh263@gmail.com>
2026-04-08 15:01:06 +0700

    "Initial WarehouseDbContext"

---
+++ Services/WarehouseService/Domain/Entities/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using SharedLibrary.Seedwork;
+
+namespace Domain.Entities
+{
+    public class Inventory : BaseEntity<String>
+    {
+        public string ProductId { get; set; } = string.Empty;
+        public string WarehouseId { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public virtual Warehouse Warehouse { get; set; }
+    }
+}
---
+++ Services/WarehouseService/Infrastructure/Data/WarehouseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public class WarehouseDbContext : DbContext
+    {
+        public WarehouseDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Warehouse> Warehouses { get; set; }
+        public virtual DbSet<Inventory> Inventories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Warehouse>(entity => {
+                entity.Property(e => e.CreatedAt).HasDefaultValueSql("now()");
+            });
+        }
+    }
+}
