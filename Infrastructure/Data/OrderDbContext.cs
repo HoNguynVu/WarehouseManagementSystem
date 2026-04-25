f4daf784ce38f365630f8fdf69030c28f5d0c66c
Hồ Nguyên Vũ <23521805@gm.uit.edu.vn>
2026-04-25 11:14:13 +0700

    add domain and dbcontext

---
+++ Infrastructure/Data/OrderDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public partial class OrderDbContext : DbContext
+    {
+        public OrderDbContext()
+        {
+
+        }
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.ToTable("Orders");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            });
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.ToTable("OrderItems");
+                entity.HasKey(e => e.Id);
+                entity.HasOne(e => e.Order)
+                    .WithMany(o => o.OrderItems)
+                    .HasForeignKey(e => e.OrderId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
---
+++ Services/OrderService/Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using SharedLibrary.Seedwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Order : BaseEntity<string>
+    {
+        public string AccountId { get; set; } = string.Empty; 
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = OrderStatus.Pending;
+        public string PaymentMethod { get; set; } = "ZaloPay";
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
